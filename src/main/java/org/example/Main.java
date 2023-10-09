32f1c20cfe524960dfb3996500757583ae457458
Victoria Markova <kistoriana@gmail.com>
2023-10-09 18:07:58 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,62 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = new int[100000];
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * 100);
+        }
+
+        // На 1000 элементов выполняется за 2 миллисекунды
+        // На 10_000 элементов выполняется за 9-11 миллисекунды
+        // На 100_000 элементов выполняется за 570-580 миллисекунд
+
+//        long m = System.currentTimeMillis();
+//        insertionSort(arr);
+//        System.out.println((double) (System.currentTimeMillis() - m));
+
+        // На 1000 элементов выполняется за 2-4 миллисекунды
+        // На 10_000 элементов выполняется за 29-33 миллисекунды
+        // На 100_000 элементов выполняется за 2560-2570 миллисекунды
+
+//        long n = System.currentTimeMillis();
+//        selectionSort(arr);
+//        System.out.println((double) (System.currentTimeMillis() - n));
+
+        // На 1000 элементов выполняется за 1 миллисекунду
+        // На 10_000 элементов выполняется за 2 миллисекунды
+        // На 100_000 элементов выполняется за 8-13 миллисекунды
+
+        long x = System.currentTimeMillis();
+        Arrays.sort(arr);
+        System.out.println((double) (System.currentTimeMillis() - x));
+    }
+
+    public static void insertionSort(int[] sortArr) {
+        int j;
+        for(int i = 1; i < sortArr.length; i++) {
+            int swap = sortArr[i];
+            for (j = i; j > 0 && swap < sortArr[j - 1]; j--) {
+                sortArr[j] = sortArr[j - 1];
+            }
+            sortArr[j] = swap;
+        }
+    }
+
+    public static void selectionSort(int[] sortArr) {
+        for(int i = 0; i < sortArr.length; i++) {
+            int pos = i;
+            int min = sortArr[i];
+            for (int j = i + 1; j < sortArr.length; j++) {
+                if(sortArr[j] < min) {
+                    pos = j;
+                    min = sortArr[j];
+                }
+            }
+            sortArr[pos] = sortArr[i];
+            sortArr[i] = min;
+        }
+    }
+}
